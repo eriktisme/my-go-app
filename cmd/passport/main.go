d74d7c9070c1fdc51c0d0b5ce3fd06088c36a448
Erik van Dam <erikvandam93@gmail.com>
2021-01-27 23:02:22 +0100

    Fix issue with CORS origin is not allowed

--- cmd/passport/main.go
+++ cmd/passport/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	handler "github.com/eriktisme/passport/internal/router"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
@@ -11,6 +13,16 @@ func main() {
 
 func SetupServer() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowAllOrigins:  false,
+		AllowOriginFunc:  func(origin string) bool { return true },
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.GET("/ping", handler.Ping)
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/eriktisme/passport
 go 1.15
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
@@ -15,6 +16,5 @@ require (
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/sys v0.0.0-20210123231150-1d476976d117 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
